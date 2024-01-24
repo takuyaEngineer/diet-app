f5660d8add507d56bebaefa7fab7929fe481cc39
takuyaEngineer <engineer.memorandum@gmail.com>
2024-01-24 18:58:40 +0900

    css修正

---
+++ src/components/pages/top/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./top.module.scss"
+import { Header } from "../../primaries/header"
+import { Calender } from "../../primaries/calender"
+
+function Top() {
+    return (
+        <div className={styles.Top}>
+            <Header></Header>
+            <Calender></Calender>
+        </div>
+    )
+}
+
+export default Top
